451f64ed84e266243c62d4386e7a5728db3a01e2
MageLink <mage.link.zhezard@gmail.com>
2022-11-26 13:45:52 -0300

    On cascade work and almost everything. This is a backup

--- prueba.sql
+++ prueba.sql
@@ -81,9 +81,11 @@ CREATE TABLE users
 CREATE TABLE answers
 (
     answers_uid UUID NOT NULL PRIMARY KEY,
-    users_uid UUID REFERENCES users(users_uid),
-    questions_uid UUID REFERENCES questions(questions_uid),
-    answer VARCHAR(255) NOT NULL
+    users_uid UUID NOT NULL,
+    questions_uid UUID NOT NULL,
+    answer VARCHAR(255) NOT NULL,
+    CONSTRAINT FK_users_uid FOREIGN KEY (users_uid) REFERENCES users(users_uid),
+    CONSTRAINT FK_questions_uid FOREIGN KEY (questions_uid) REFERENCES questions(questions_uid)
 );
 
 -- Insertando datos requeridos. 5 preguntas y 5 usuarios.
@@ -119,21 +121,28 @@ INSERT INTO users(users_uid, name, age) VALUES (
     uuid_generate_v4(), 'Camila', 35
 );
 
+-- Checkin para obtener el UUID de las preguntas y los usuarios
+SELECT * FROM users;
+SELECT * FROM questions;
+
 -- Insertando respuestas
+-- 1. Primera pregunta debe estar contestada dos veces por distintos usuarios.
+-- 2. Segunda pregunta debe estar contestada solo por un usuario.
+-- 3. Las otras dos incorrectas
 INSERT INTO answers(answers_uid, users_uid, questions_uid, answer) VALUES (
-    uuid_generate_v4(), 'a47e7781-231f-46b8-b92a-7cd8051b39dc', 'c4dc36e4-6fb2-4672-b83d-22c17748d74e', 'Si pueden'
+    uuid_generate_v4(), '4cd69cff-3851-48dc-b3f9-5e5832d9f389', '31cd6e0b-6268-4446-9403-ee638873323a', 'Si pueden'
 );
 INSERT INTO answers(answers_uid, users_uid, questions_uid, answer) VALUES (
-    uuid_generate_v4(), 'fa9ce993-0b87-4791-9055-deff001bfa39', 'c4dc36e4-6fb2-4672-b83d-22c17748d74e', 'Si pueden'
+    uuid_generate_v4(), 'dc615714-32d9-481a-98ab-4e60d8f9887c', '31cd6e0b-6268-4446-9403-ee638873323a', 'Si pueden'
 );
 INSERT INTO answers(answers_uid, users_uid, questions_uid, answer) VALUES (
-    uuid_generate_v4(), '0ef39aed-6d26-4a9f-a419-8c477f7e29a3', 'd500fb9a-064b-4d98-8de1-86fa2e84bd23', 'Entre sus partículas, si tienen'
+    uuid_generate_v4(), '78166d3e-bba4-4d3d-9fe8-f43798ad97da', 'f09e51b7-1944-467d-b891-3a2642942ebb', 'Entre sus partículas, si tienen'
 );
 INSERT INTO answers(answers_uid, users_uid, questions_uid, answer) VALUES (
-    uuid_generate_v4(), '3e40d471-89e1-4ba2-a0fb-3b3c8fbd8681', 'c2515184-db0a-48b7-8042-6e15830e2f7b', 'No lo sé'
+    uuid_generate_v4(), '17d2fc9d-b73f-4b29-a2d3-50c216c741bd', '608b45ff-471b-405d-8298-a5b066117e3f', 'No lo sé'
 );
 INSERT INTO answers(answers_uid, users_uid, questions_uid, answer) VALUES (
-    uuid_generate_v4(), '1bac9f02-5cf9-4816-8847-3e04fb375557', '920f8201-4371-4fed-bd55-d5aa85b93e74', 'No sé :c'
+    uuid_generate_v4(), 'd08f6034-b60d-46e1-aa67-b9936ffba4ea', 'c8a0439d-3a6d-4a31-9f2b-1dde95803e2a', 'No sé :c'
 );
 
 -- Checkin
@@ -142,19 +151,43 @@ SELECT * FROM users;
 SELECT * FROM answers;
 
 --- Cuenta la cantidad de respuestas correctas totales por usuario (independiente de la pregunta).
-SELECT users.name, questions.right_answer, COUNT(*) FROM answers
+SELECT users.name, COUNT(*) AS total_right_answers FROM answers
 LEFT JOIN users USING (users_uid)
-LEFT JOIN questions USING (questions_uid) WHERE questions.right_answer = answer
-GROUP BY users.name, questions.right_answer;
+LEFT JOIN questions USING (questions_uid) 
+WHERE questions.right_answer = answer
+GROUP BY users.name;
+
+
+SELECT users.name FROM answers
+LEFT JOIN users USING (users_uid)
+LEFT JOIN questions USING (questions_uid);
 
 --- Por cada pregunta, en la tabla preguntas, cuenta cuántos usuarios tuvieron la respuesta correcta.
 SELECT questions.question, COUNT(*) AS total_rights_answer_users FROM answers
 LEFT JOIN users USING (users_uid)
-LEFT JOIN questions USING (questions_uid) WHERE questions.right_answer = answer
+LEFT JOIN questions USING (questions_uid) 
+WHERE questions.right_answer = answer
 GROUP BY questions.question;
 
 --- Implementa borrado en cascada de las respuestas al borrar un usuario y borrar 
 --- el primer usuario para probar la implementación. 
+-- CHECK
+SELECT * FROM users;
+
+SELECT users.name FROM answers 
+LEFT JOIN users USING (users_uid);
+
+DELETE FROM users WHERE users_uid = '4cd69cff-3851-48dc-b3f9-5e5832d9f389';
+
+-- Procedemos
+
+ALTER TABLE answers DROP CONSTRAINT FK_users_uid;
+-- Ahora
+ALTER TABLE answers 
+ADD CONSTRAINT FK_users_uid 
+FOREIGN KEY (users_uid) 
+REFERENCES users(users_uid)
+ON DELETE CASCADE;
 
 --- Crea una restricción que impida insertar usuarios menores de 18 años en la base de datos.
 ALTER TABLE users ADD CONSTRAINT users_age_contraint CHECK (age > 17);
